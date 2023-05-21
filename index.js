73268978ae0fd52ac86d3cc595fefb457ab83001
tluwaniMS <tluwani2020@gmail.com>
2023-05-21 12:44:11 +0200

    adding-index-js

---
+++ index.js
@@ -0,0 +1,6 @@
+const name = process.env.NAME || 'John'
+const surname = process.env.SURNAME || 'Doe'
+
+const message = `Greetings to you ${name} ${surname}, and welcome to the chocolate factory!!!`;
+
+console.log(message);
